44f529d4bca1c4328fe30e08420150c6debd13d2
ColdBeer <bryanlihp@hotmail.com>
2018-06-01 21:59:54 -0700

    Create demo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+import React from "react"
+import ReactDOM from "react-dom"
+
+class App extends React.Component {
+    render() {
+        return <div> This is a test </div>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("content"));
